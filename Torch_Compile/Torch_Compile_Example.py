78b7cff982e8af3ede2c6e5efa313a557852d981
Muhammad Attiq <muhammad.attiq.computer.engineer@gmail.com>
2026-01-31 16:15:42 +0500

    Create Torch_Compile_Example.py

---
+++ Torch_Compile/Torch_Compile_Example.py
@@ -0,0 +1,18 @@
+import torch
+torch._logging.set_logs(graph_code=True)
+
+def foo(x, y):
+    a = torch.sin(x)
+    b = torch.cos(y)
+    return a + b
+
+opt_foo1 = torch.compile(foo)
+print(opt_foo1(torch.randn(3, 3), torch.randn(3, 3)))
+
+@torch.compile
+def opt_foo2(x, y):
+    a = torch.sin(x)
+    b = torch.cos(y)
+    return a + b
+
+print(opt_foo2(torch.randn(3, 3), torch.randn(3, 3)))
